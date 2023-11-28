a8c3bf6abfc7f2d480ff9970d7554d3aa7db4606
kriscCodes <110057928+kriscCodes@users.noreply.github.com>
2023-11-28 13:52:15 -0500

    handeling CORS

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
